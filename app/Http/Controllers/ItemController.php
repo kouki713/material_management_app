55b085c8c8be38726e2608f7ccd13d85097b54f4
k.sawada <63055099+koukisawada1@users.noreply.github.com>
2020-10-23 15:55:18 +0900

    Add:Itemコントローラ作成

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,7 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Item;
+use App\Models\Receipt;
+use App\Models\Purchase;
+use App\Models\Allocate;
+
+//バリデーション設定ファイルの読み込み
+use App\Http\Requests\StoreItemForm;
+
+use Illuminate\support\Facades\DB;
+
 class ItemController extends Controller
 {
-    //
+    public function index() {
+
+        $items = Item::paginate(5);
+
+        
+        return view('items.index', compact('items'));
+    }
+
+    public function create() {
+        return view('items.create');
+    }
+
+    public function store(StoreItemForm $request) {
+        $item = new Item;
+
+        $item->item_name = $request->input('item_name');
+
+        
+        $item->save();
+        
+        return redirect('item/index')->with('message', '部材を登録しました。');
+    
+    }
 }
