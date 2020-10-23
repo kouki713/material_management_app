cf3089318c2603439377002ce416b7100d88f554
k.sawada <63055099+koukisawada1@users.noreply.github.com>
2020-10-23 15:31:26 +0900

    Add:マイグレーションファイルを追加

---
+++ database/migrations/2020_10_23_145619_create_allocates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAllocatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('allocates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('item_id');
+            $table->string('name', 50);
+            $table->integer('num');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('allocates');
+    }
+}
