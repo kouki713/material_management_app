20fafd8d2a9f8812de1230d07c9bb869098fd54c
k.sawada <63055099+koukisawada1@users.noreply.github.com>
2020-10-23 17:27:47 +0900

    Add:部材入庫登録ページ作成

---
+++ resources/views/receipts/create.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">部材入庫画面</div>
+                <div class="card-body">
+                    <form method="POST" action="{{route('receipt.store')}}">
+                    @csrf
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td>{{ $item->item_name }}</th>
+                                <td><input type="number" name="num" min="1" max="1000" class="form-control"></td>
+                                <td>未入庫　{{ $num }} 個</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <input type="hidden" name="item_id" value="{{ $item->id }}">
+
+                    <input type="submit" class="btn btn-info text-right" value="入庫">
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-10" style="margin-top: 50px;">
+            <div class="card">
+                <div class="card-header">入庫履歴</div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>部材名</th>
+                                <th>入庫日</th>
+                                <th>入庫個数</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($item->receipts as $receipt)
+                                <tr>
+                                    <td>{{ $receipt->item->item_name }}</td>
+                                    <td>{{ $receipt->created_at }}</td>
+                                    <td>{{ $receipt->num }} 個</td>
+                                </tr>
+                            @endforeach
+                        </tbody>    
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
